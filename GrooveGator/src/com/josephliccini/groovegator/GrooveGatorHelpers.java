18749a0476b6bc08cda115f1a5e000a68e460efe
Joseph Liccini <joseph.liccini@ufl.edu>
2014-06-20 23:29:46 -0700

    Major bugfix with downloading in wrong directory due to '/' in the album/artist/song name

--- GrooveGator/src/com/josephliccini/groovegator/GrooveGatorHelpers.java
+++ GrooveGator/src/com/josephliccini/groovegator/GrooveGatorHelpers.java
@@ -78,38 +78,52 @@ public class GrooveGatorHelpers {
 		Preferences prefs = Preferences.userRoot().node(GrooveGator.class.getName());
 		String prefString = prefs.get("OutputFormat", "<artist> - <song> - <album>");
 		prefString = StringUtils.replace(prefString, "\\", "/");
-		prefString = StringUtils.replace(prefString, "<artist>", song.ArtistName);
-		prefString = StringUtils.replace(prefString, "<song>", song.Name);
-		prefString = StringUtils.replace(prefString, "<album>", song.AlbumName);
-		prefString = StringUtils.replace(prefString, "<year>", song.Year);
-		prefString = StringUtils.replace(prefString, "<tracknum>", song.TrackNum);
-		String filename, path = "";
-		if (prefString.lastIndexOf("/") > 0)
+		int index;
+		String filename,path = "";
+		if ((index = prefString.lastIndexOf("/")) > 0)
 		{
-			path = prefString.substring(0, prefString.lastIndexOf("/"));
-			filename = prefString.substring(prefString.lastIndexOf("/")+1, prefString.length());
-			System.out.println("Filename: " + filename);
-			System.out.println("Path: " + path);
+			filename = prefString.substring(index+1);
+			filename = StringUtils.replace(filename, "<artist>", StringUtils.remove(StringUtils.remove(song.ArtistName, '/'), '\\'));
+			filename = StringUtils.replace(filename, "<song>", StringUtils.remove(StringUtils.remove(song.SongName, '/'), '\\'));
+			filename = StringUtils.replace(filename, "<album>", StringUtils.remove(StringUtils.remove(song.AlbumName, '/'), '\\'));
+			filename = StringUtils.replace(filename, "<year>", StringUtils.remove(StringUtils.remove(song.Year, '/'), '\\'));
+			filename = StringUtils.replace(filename, "<tracknum>", StringUtils.remove(StringUtils.remove(song.TrackNum, '/'), '\\'));
+			
+			path = prefString.substring(0, index);
+			path = StringUtils.replace(path, "<artist>", StringUtils.remove(StringUtils.remove(song.ArtistName, '/'), '\\'));
+			path = StringUtils.replace(path, "<song>", StringUtils.remove(StringUtils.remove(song.SongName, '/'), '\\'));
+			path = StringUtils.replace(path, "<album>", StringUtils.remove(StringUtils.remove(song.AlbumName, '/'), '\\'));
+			path = StringUtils.replace(path, "<year>", StringUtils.remove(StringUtils.remove(song.Year, '/'), '\\'));
+			path = StringUtils.replace(path, "<tracknum>", StringUtils.remove(StringUtils.remove(song.TrackNum, '/'), '\\'));
 		}
 		else
 		{
-			filename = prefString; 
+			prefString = StringUtils.replace(prefString, "<artist>", StringUtils.remove(StringUtils.remove(song.ArtistName, '/'), '\\'));
+			prefString = StringUtils.replace(prefString, "<song>", StringUtils.remove(StringUtils.remove(song.SongName, '/'), '\\'));
+			prefString = StringUtils.replace(prefString, "<album>", StringUtils.remove(StringUtils.remove(song.AlbumName, '/'), '\\'));
+			prefString = StringUtils.replace(prefString, "<year>", StringUtils.remove(StringUtils.remove(song.Year, '/'), '\\'));
+			prefString = StringUtils.replace(prefString, "<tracknum>", StringUtils.remove(StringUtils.remove(song.TrackNum, '/'), '\\'));
+			filename = prefString;
 		}
-		String tmpString = "";
-		for (int i=0; i<filename.length(); i++) 
+		System.out.println("Filename: " + filename);
+		System.out.println("Path: " + path);
+		
+		StringBuilder builder = new StringBuilder("");
+		for (int i=0; i<filename.length() && i < 255; ++i) 
 		{
 			try {
 				new File(filename.charAt(i) + "").getCanonicalFile();
 				if (filename.charAt(i) != '/' && filename.charAt(i) != '\\') 
 				{
-					tmpString +=  filename.charAt(i);
+					builder.append(filename.charAt(i));
 				}
 			} catch(Exception ex) {System.out.println("Exception"); }
 		}
+
 		String rootDownloadDirectoryPath = prefs.get("OutputDirectory", System.getProperty("user.dir"));
-		File pathDirectory = new File(rootDownloadDirectoryPath + "/" + path);
+		File pathDirectory = new File(rootDownloadDirectoryPath, path);
 		pathDirectory.mkdirs();
-		return path + "/" +  tmpString.replace("\"", "");
+		return path + "/" +  builder.toString().replace("\"", "");
 	}
 	
 	public static GroovesharkClient getClient()
